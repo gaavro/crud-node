710506cfd86388651e27c2a1b9e499d87d5abcf4
Gabriela <gabrielarodrigues432@gmail.com>
2022-01-11 16:35:13 -0300

    inicial commit

---
+++ src/index.js
@@ -0,0 +1,143 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+const USERS = [];
+
+app.use(express.json());
+const verifyCPF = (req, res, next) => {
+  const { cpf } = req.params;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  if (user === undefined) {
+    return res.status(404).json({ message: "CPF not registered!" });
+  }
+  next();
+};
+
+
+const verifyId = (req, res, next) => {
+  const { cpf, id } = req.params;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  const note = user.notes.find((item) => item.id === id);
+
+  if (note === undefined) {
+    return res
+      .status(404)
+      .json({ message: "Invalid ID. Note not registered!" });
+  }
+  next();
+};
+
+app.get("/users", (req, res) => {
+  res.json(USERS);
+});
+
+app.post("/users", (req, res) => {
+  const data = req.body;
+  data.id = uuidv4();
+  data.notes = [];
+  USERS.push(data);
+  res.status(201).json(data);
+});
+
+app.patch("/users/:cpf", verifyCPF, (req, res) => {
+  const { cpf } = req.params;
+  const data = req.body;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  user.cpf = data.cpf;
+  user.name = data.name;
+  res.json({ message: "User is updated", user });
+
+});
+
+
+app.delete("/users/:cpf", verifyCPF, (req, res) => {
+  const { cpf } = req.params;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  const index = USERS.indexOf(user);
+  USERS.splice(index, 1);
+  res.status(200).json({
+    message: "User is deleted",
+    updated_list: USERS.splice(index, 1),
+  });
+
+
+});
+
+app.get("/users/:cpf/notes", verifyCPF, (req, res) => {
+  const { cpf } = req.params;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  res.json(user.notes);
+});
+
+
+app.post("/users/:cpf/notes", verifyCPF, (req, res) => {
+  const { cpf } = req.params;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+  const data = req.body;
+  data.id = uuidv4();
+  data.created_at = new Date();
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  user.notes.push(data);
+  res.status(201).json({
+    message: `${data.title} was added into ${user.name}'s notes`,
+  });
+});
+
+
+app.patch("/users/:cpf/notes/:id", verifyCPF, verifyId, (req, res) => {
+  const { cpf, id } = req.params;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+  const data = req.body;
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  const note = user.notes.find((item) => item.id === id);
+  note.title = data.title;
+  note.content = data.content;
+  note.updated_at = new Date();
+  res.json(note);
+
+});
+
+
+app.delete("/users/:cpf/notes/:id", verifyCPF, verifyId, (req, res) => {
+  const { cpf, id } = req.params;
+  const formattedCPF = cpf.replace(
+    /(\d{3})(\d{3})(\d{3})(\d{2})/,
+    "$1.$2.$3-$4"
+  );
+  const user = USERS.find((item) => item.cpf === formattedCPF);
+  const note = user.notes.find((item) => item.id === id);
+  const index = user.notes.indexOf(note);
+  user.notes.splice(index, 1);
+  res.status(200).json(user.notes);
+});
+
+
+app.listen(3001);
